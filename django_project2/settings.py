44b1232daf842bf50a1b4658820d1cc0bb4b51f1
UTKARSH <utkarshsrireal@gmail.com>
2024-12-02 13:00:29 +0530

    settings updated

--- django_project2/settings.py
+++ django_project2/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR,'secret_key.txt')) as f:
-    SECRET_KEY=f.read().strip()
+SECRET_KEY=os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://post-coast-cad73a.netlify.app/', '127.0.0.1']
+ALLOWED_HOSTS = ['https://post-coast.netlify.app/', '127.0.0.1']
 
 
 # Application definition
